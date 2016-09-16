1c7a34d09691c4d4279a81310b5b32fd88911798
John Kennedy <johnwjken@gmail.com>
2016-09-16 11:49:40 +0100

    Created sign up page

---
+++ app/src/main/java/app/Signup.java
@@ -0,0 +1,15 @@
+package app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.donation.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
