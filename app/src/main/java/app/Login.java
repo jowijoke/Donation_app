7f687be2917bed31086814fdd17f25e16d4eca6c
John Kennedy <johnwjken@gmail.com>
2016-09-17 11:55:51 +0100

    Validated registered Users at Login

--- app/src/main/java/app/Login.java
+++ app/src/main/java/app/Login.java
@@ -1,9 +1,11 @@
 package app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import app.donation.R;
 
@@ -17,6 +19,20 @@ public class Login extends AppCompatActivity {
 
     public void signInPressed (View view)
     {
-        startActivity (new Intent(this, Donate.class));
+            DonationApp app = (DonationApp) getApplication();
+
+            TextView email     = (TextView)  findViewById(R.id.loginEmail);
+            TextView password  = (TextView)  findViewById(R.id.loginPassword);
+
+            if (app.validUser(email.getText().toString(), password.getText().toString()))
+            {
+                startActivity (new Intent(this, Donate.class));
+            }
+            else
+            {
+                Toast toast = Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
     }
 }
