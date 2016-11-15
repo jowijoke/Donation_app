1d36c085af1b0c337d10842c140ecb7882a042e4
John Kennedy <johnwjken@gmail.com>
2016-11-15 11:00:48 +0000

    Modified DonationApp.java

--- app/src/main/java/app/main/DonationApp.java
+++ app/src/main/java/app/main/DonationApp.java
@@ -4,19 +4,31 @@ import android.app.Application;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import app.model.Candidate;
 import app.model.Donation;
 import app.model.User;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DonationApp extends Application
 {
-    public List <User> users = new ArrayList<User>();
+    public DonationService donationService;
+    public boolean donationServiceAvailable = false;
+    public String service_url = "http://10.0.2.2:4000";   // Standard Emulator IP Address
 
     public final int       target       = 10000;
     public int             totalDonated = 0;
-    public List<Donation> donations    = new ArrayList<Donation>();
+
+    public User currentUser;
+    public List<Donation> donations = new ArrayList<Donation>();
+    public List<User> users = new ArrayList<User>();
+    public List<Candidate> candidates = new ArrayList<Candidate>();
 
     public boolean newDonation(Donation donation)
     {
@@ -34,6 +46,21 @@ public class DonationApp extends Application
         return targetAchieved;
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        super.onCreate();
+        Gson gson = new GsonBuilder().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(service_url)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        donationService = retrofit.create(DonationService.class);
+
+        Log.v("Donation", "Donation App Started");
+    }
+
     public void newUser(User user)
     {
         users.add(user);
@@ -45,16 +72,10 @@ public class DonationApp extends Application
         {
             if (user.email.equals(email) && user.password.equals(password))
             {
+                currentUser = user;
                 return true;
             }
         }
         return false;
-    }
-
-    @Override
-    public void onCreate()
-    {
-        super.onCreate();
-        Log.v("Donate", "Donation App Started");
     }
 }
